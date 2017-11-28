573b8783d658502918d872d2488df899f2bba47e
Markus Mühlbrandt <muehlbrandt@itemis.de>
2017-11-28 12:52:27 +0100

    Added test suite.

---
+++ org.genivi.commonapi.wamp.tests/src/org/genivi/commonapi/wamp/tests/WampGeneratorTestSuite.java
@@ -0,0 +1,19 @@
+package org.genivi.commonapi.wamp.tests;
+
+import org.genivi.commonapi.wamp.tests.runtime.Example10Test;
+import org.genivi.commonapi.wamp.tests.runtime.Example12Test;
+import org.genivi.commonapi.wamp.tests.runtime.Example77Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ //
+		WampGeneratorTests.class, //
+		Example10Test.class, //
+		Example12Test.class, //
+		Example77Test.class, //
+})
+public class WampGeneratorTestSuite {
+
+}
