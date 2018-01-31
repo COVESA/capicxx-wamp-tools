f2e04efc305ba5477d6d7f8778c7be95b76e00e4
Markus Mühlbrandt <muehlbrandt@itemis.de>
2018-01-31 16:39:16 +0100

    Refactoring of cli options.

--- org.genivi.commonapi.wamp.cli/src/org/genivi/commonapi/wamp/cli/WampGeneratorCommandLineTool.java
+++ org.genivi.commonapi.wamp.cli/src/org/genivi/commonapi/wamp/cli/WampGeneratorCommandLineTool.java
@@ -1,7 +1,6 @@
 package org.genivi.commonapi.wamp.cli;
 
 import java.io.File;
-import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
@@ -10,35 +9,45 @@ import org.apache.commons.cli.Options;
 import org.apache.log4j.Logger;
 import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
-import org.eclipse.xtext.generator.OutputConfiguration;
 import org.franca.core.franca.FModel;
 import org.genivi.commonapi.wamp.WampGeneratorFrancaStandaloneSetup;
+import org.genivi.commonapi.wamp.preferences.FPreferencesWamp;
+import org.genivi.commonapi.wamp.preferences.PreferenceConstantsWamp;
 
-import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 
 /**
  * Runner for standalone CommonAPI/WAMP generation from Franca files.
- *  
- * @author Klaus Birken (itemis)
+ * 
+ * @author Klaus Birken (itemis), Markus Mühlbrandt (itemis)
  */
+@SuppressWarnings("deprecation")
 public class WampGeneratorCommandLineTool extends AbstractCommandLineTool {
-
-	// prepare class for logging....
-	private static final Logger logger = Logger.getLogger(WampGeneratorCommandLineTool.class);
-
+	
 	private static final String TOOL_VERSION = "0.7.0";
 
 	// specific option values
 	private static final String FIDLFILE = "f";
-	private static final String RECURSIVE_VALIDATION = "r";
-	
+	private static final String OPT_RECURSIVE_VALIDATION = "r";
+	private static final String OPT_RECURSIVE_VALIDATION_LONG = "recursive-validation";
+	private static final String OPT_LOG_LEVEL = "ll";
+	private static final String OPT_LOG_LEVEL_LONG = "loglevel";
+	private static final String OPT_OUTDIR = "d";
+	private static final String OPT_OUTDIR_LONG = "dest";
+
+	// prepare class for logging....
+	private static final Logger logger = Logger.getLogger(WampGeneratorCommandLineTool.class);
+	private boolean logInfo = true;
+	private boolean logError = true;
+
 	/**
-	 * The main function for this standalone tool.</p>
+	 * The main function for this standalone tool.
+	 * </p>
 	 * 
 	 * It directly hands over control to the CommandLineTool framework.
 	 * 
-	 * @param args the collection of command line arguments
+	 * @param args
+	 *            the collection of command line arguments
 	 */
 	public static void main(String[] args) {
 		// hand over control to CommandLineTool framework
@@ -46,86 +55,117 @@ public class WampGeneratorCommandLineTool extends AbstractCommandLineTool {
 	}
 
 	// injected fragments
-	@Inject IGenerator generator;
+	@Inject
+	IGenerator generator;
 
 	@Override
 	protected int run(CommandLine line) {
 		// print version string
-//		System.out.println(VERSIONSTR);
+		// System.out.println(TOOL_VERSION);
 
-		// TODO configure Logger depends on given verbose value
-//		boolean verbose = line.hasOption(VERBOSE);
+		if (line.hasOption(OPT_LOG_LEVEL)) {
+			setLogLevel(line.getOptionValue(OPT_LOG_LEVEL));
+		}
 		
+		if (line.hasOption(CommonOptions.VERBOSE)) {
+			setLogLevel(PreferenceConstantsWamp.LOGLEVEL_VERBOSE);
+		}
+
 		// load Franca IDL file
 		String fidlFile = line.getOptionValue(FIDLFILE);
 		FModel fmodel = persistenceManager.loadModel(fidlFile);
-		if (fmodel==null) {
+		if (fmodel == null) {
 			logError("Couldn't load Franca IDL file '" + fidlFile + "'.");
 			return -1;
 		}
-//		logger.info("Franca IDL: package '" + fmodel.getName() + "'");
 
 		// call validator
-		int nErrors = validateModel(fmodel, line.hasOption(RECURSIVE_VALIDATION));
-		if (nErrors>0) {
-			System.err.println("Validation of Franca model: " + nErrors + " errors, aborting.");
+		int nErrors = validateModel(fmodel, line.hasOption(OPT_RECURSIVE_VALIDATION));
+		if (nErrors > 0) {
+			logError("Validation of Franca model: " + nErrors + " errors, aborting.");
 			return -1;
 		}
-		
+
+		FPreferencesWamp preferences = FPreferencesWamp.getInstance();
+		if (line.hasOption(OPT_OUTDIR)) {
+			setDefaultDirectory(line.getOptionValue(OPT_OUTDIR));
+		}
+
 		// call generator and save files
 		JavaIoFileSystemAccess fsa = injector.getInstance(JavaIoFileSystemAccess.class);
-		if (line.hasOption(CommonOptions.OUTDIR)) {
-			String outPath = line.getOptionValue(CommonOptions.OUTDIR);
-			Map<String, OutputConfiguration> configs = Maps.newHashMap();
-			addOutputConfig(configs, fsa.DEFAULT_OUTPUT, outPath);
-			addOutputConfig(configs, "outputDirProxies", outPath);
-			addOutputConfig(configs, "outputDirStubs", outPath);
-			fsa.setOutputConfigurations(configs);
-		}
+		fsa.setOutputConfigurations(preferences.getOutputpathConfiguration());
+		logInfo(String.format("Generating \"%s\".", fidlFile));
 		generator.doGenerate(fmodel.eResource(), fsa);
-
-//		logger.info("FrancaStandaloneGen done.");
+		logInfo(String.format("\"%s\" generation finished.", fidlFile));
 		return 0;
 	}
-	
-	private void addOutputConfig(Map<String, OutputConfiguration> configs, String name, String path) {
-		OutputConfiguration cfg = new OutputConfiguration(name);
-		cfg.setCreateOutputDirectory(true);
-		cfg.setOutputDirectory(path);
-		configs.put(name, cfg);
+
+	@Override
+	protected void addOptions(Options options) {
+		// required
+		options.addOption(createFileOption());
+
+		// option for configuration of an output directory
+		options.addOption(createOutputDirOption());
+
+		options.addOption(createLogLevelOption());
+
+		options.addOption(createRecursiveValidationOption());
 	}
 
 	@SuppressWarnings("static-access")
-	@Override
-	protected void addOptions(Options options) {
-		// String[] set = LogFactory.getLog(getClass()).
+	private Option createRecursiveValidationOption() {
+		return OptionBuilder//
+				.withArgName("recval")//
+				.withDescription("Recursive validation")//
+				.hasArg(false)//
+				.isRequired(false)//
+				.withLongOpt(OPT_RECURSIVE_VALIDATION_LONG) //
+				.create(OPT_RECURSIVE_VALIDATION);
+	}
 
-		// required
-		Option optInputFidl = OptionBuilder.withArgName("Franca IDL file")
-				.withDescription("Input file in Franca IDL (fidl) format.").hasArg().isRequired()
-				.withValueSeparator(' ').create(FIDLFILE);
-		//optInputFidl.setType(File.class);
-		options.addOption(optInputFidl);
+	@SuppressWarnings("static-access")
+	private Option createLogLevelOption() {
+		return OptionBuilder//
+				.withArgName("arg") //
+				.withDescription("The log level (quiet or verbose)") //
+				.hasArg() //
+				.withValueSeparator(' ') //
+				.withLongOpt(OPT_LOG_LEVEL_LONG) //
+				.create(OPT_LOG_LEVEL);
+	}
 
-		// option for configuration of an output directory 
-		CommonOptions.createOutdirOption(options);
+	@SuppressWarnings("static-access")
+	private Option createFileOption() {
+		return OptionBuilder.withArgName("Franca IDL file").withDescription("Input file in Franca IDL (fidl) format.")
+				.hasArg().isRequired().withValueSeparator(' ').create(FIDLFILE);
+	}
 
-		Option optRecursiveValidation = OptionBuilder.withArgName("recval")
-		.withDescription("Recursive validation").hasArg(false)
-		.isRequired(false).create(RECURSIVE_VALIDATION);
-		options.addOption(optRecursiveValidation);
+	@SuppressWarnings("static-access")
+	private Option createOutputDirOption() {
+		return OptionBuilder//
+				.withArgName("directory") //
+				.withDescription("Set output directory") //
+				.hasArg() //
+				.withValueSeparator(' ') //
+				.withLongOpt(OPT_OUTDIR_LONG) //
+				.create(OPT_OUTDIR);
 	}
 
 	@Override
 	protected void logError(String message) {
-		logger.error(message);
+		if (logError) {
+			logger.error(message);
+		}
 	}
-	
+
 	@Override
 	protected void logInfo(String message) {
-		logger.info(message);
+		if (logInfo) {
+			logger.info(message);
+		}
 	}
-	
+
 	@Override
 	protected boolean checkCommandLineValues(CommandLine line) {
 		if (line.hasOption(FIDLFILE)) {
@@ -137,6 +177,51 @@ public class WampGeneratorCommandLineTool extends AbstractCommandLineTool {
 				logError("Cannot open Franca IDL file '" + fidlFile + "'.");
 			}
 		}
+		// TODO: Add validation for outdir
+		// TODO: Validation for loglevel -> check -verbose & -ll quiet (illegal arguments) 
 		return false;
+	}
+
+	private FPreferencesWamp getPreferences() {
+		return FPreferencesWamp.getInstance();
+	}
+
+	private void setDefaultDirectory(String optionValue) {
+		logInfo("Default output directory: " + optionValue);
+
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP, optionValue);
+		// In the case where no other output directories are set,
+		// this default directory will be used for them
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP, optionValue);
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, optionValue);
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, optionValue);
+	}
+
+	public void setCommonDirectory(String optionValue) {
+		logInfo("Common output directory: " + optionValue);
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP, optionValue);
+	}
+
+	public void setProxyDirectory(String optionValue) {
+		logInfo("Proxy output directory: " + optionValue);
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, optionValue);
+	}
+
+	public void setStubDirectory(String optionValue) {
+		logInfo("Stub output directory: " + optionValue);
+		getPreferences().setPreference(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, optionValue);
+	}
+
+	public void setLogLevel(String optionValue) {
+		if (PreferenceConstantsWamp.LOGLEVEL_QUIET.equals(optionValue)) {
+			getPreferences().setPreference(PreferenceConstantsWamp.P_LOGOUTPUT_WAMP, "false");
+			logInfo = false;
+			logError = false;
+		}
+		if (PreferenceConstantsWamp.LOGLEVEL_VERBOSE.equals(optionValue)) {
+			getPreferences().setPreference(PreferenceConstantsWamp.P_LOGOUTPUT_WAMP, "true");
+			logInfo = true;
+			logError = true;
+		}
 	}
 }
--- org.genivi.commonapi.wamp/src/org/genivi/commonapi/wamp/preferences/FPreferencesWamp.java
+++ org.genivi.commonapi.wamp/src/org/genivi/commonapi/wamp/preferences/FPreferencesWamp.java
@@ -12,142 +12,159 @@ import org.genivi.commonapi.wamp.preferences.PreferenceConstantsWamp;
 
 public class FPreferencesWamp {
 
-	   private static FPreferencesWamp instance = null;
-	    private Map<String, String> preferences = null;
+	private static FPreferencesWamp instance = null;
+	private Map<String, String> preferences = null;
 
-	    public Map<String, String> getPreferences() {
-			return preferences;
+	public Map<String, String> getPreferences() {
+		return preferences;
+	}
+
+	private FPreferencesWamp() {
+		preferences = new HashMap<String, String>();
+		initPreferences();
+	}
+
+	public void resetPreferences() {
+		preferences.clear();
+	}
+
+	public static FPreferencesWamp getInstance() {
+		if (instance == null) {
+			instance = new FPreferencesWamp();
+		}
+		return instance;
+	}
+
+	private void initPreferences() {
+		// -dc,--dest-common <arg> "The directory for the common code"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP, PreferenceConstantsWamp.DEFAULT_OUTPUT);
+		}
+		// -dp,--dest-proxy <arg> "The directory for proxy code"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, PreferenceConstantsWamp.DEFAULT_OUTPUT);
+		}
+		// -ds,--dest-stub <arg> "The directory for stub code"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, PreferenceConstantsWamp.DEFAULT_OUTPUT);
+		}
+		// -d,--dest <arg> "The default output directory"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP, PreferenceConstants.DEFAULT_OUTPUT);
+		}
+		// -dsub,--dest-subdirs "Use subdir per interface"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_SUBDIRS_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_OUTPUT_SUBDIRS_WAMP, "false");
+		}
+		// -l,--license <arg> "The file path to the license text that will be added to
+		// each generated file"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_LICENSE_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_LICENSE_WAMP, PreferenceConstantsWamp.DEFAULT_LICENSE);
+		}
+		// -nc,--no-common "Switch off generation of common code"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_COMMON_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_GENERATE_COMMON_WAMP, "true");
+		}
+		// -ns,--no-stub "Switch off generation of stub code"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_STUB_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_GENERATE_STUB_WAMP, "true");
+		}
+		// -np,--no-proxy "Switch off generation of proxy code"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_PROXY_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_GENERATE_PROXY_WAMP, "true");
+		}
+		// Franca: -v / CommonAPI: -ll,--loglevel <arg> "The log level (quiet or
+		// verbose)"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_LOGOUTPUT_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_LOGOUTPUT_WAMP, "true");
+		}
+		// -ng,--no-gen "Switch off code generation"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_CODE_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_GENERATE_CODE_WAMP, "true");
+		}
+		// -wod,--without-dependencies "Switch off code generation of dependencies"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_DEPENDENCIES_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_GENERATE_DEPENDENCIES_WAMP, "true");
+		}
+		// -nsc,--no-sync-calls "Switch off code generation of synchronous methods"
+		if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_SYNC_CALLS_WAMP)) {
+			preferences.put(PreferenceConstantsWamp.P_GENERATE_SYNC_CALLS_WAMP, "true");
+		}
+	}
+
+	public String getPreference(String preferencename, String defaultValue) {
+
+		if (preferences.containsKey(preferencename)) {
+			return preferences.get(preferencename);
+		}
+		System.err.println("Unknown preference " + preferencename);
+		return "";
+	}
+
+	public void setPreference(String name, String value) {
+		if (preferences != null) {
+			preferences.put(name, value);
+		}
+	}
+
+	public String getModelPath(FModel model) {
+		String ret = model.eResource().getURI().toString();
+		return ret;
+	}
+
+	/**
+	 * Set the output path configurations (based on stored the preference values)
+	 * for file system access types (instance of AbstractFileSystemAccess)
+	 * 
+	 * @return
+	 */
+	public HashMap<String, OutputConfiguration> getOutputpathConfiguration() {
+		return getOutputpathConfiguration(null);
+	}
+
+	/**
+	 * Set the output path configurations (based on stored the preference values)
+	 * for file system access types (instance of AbstractFileSystemAccess)
+	 * 
+	 * @subdir the subdir to use, can be null
+	 * @return
+	 */
+	public HashMap<String, OutputConfiguration> getOutputpathConfiguration(String subdir) {
+
+		String defaultDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP,
+				PreferenceConstants.DEFAULT_OUTPUT);
+		String commonDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP, defaultDir);
+		String outputProxyDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, defaultDir);
+		String outputStubDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, defaultDir);
+
+		if (null != subdir && getPreference(PreferenceConstants.P_OUTPUT_SUBDIRS, "false").equals("true")) {
+			defaultDir = new File(defaultDir, subdir).getPath();
+			commonDir = new File(commonDir, subdir).getPath();
+			outputProxyDir = new File(outputProxyDir, subdir).getPath();
+			outputStubDir = new File(outputStubDir, subdir).getPath();
 		}
 
-		private FPreferencesWamp() {
-	        preferences = new HashMap<String, String>();
-	        clidefPreferences();
-	    }
+		HashMap<String, OutputConfiguration> outputs = new HashMap<String, OutputConfiguration>();
 
-	    public void resetPreferences(){
-	        preferences.clear();
-	    }
+		OutputConfiguration commonOutput = new OutputConfiguration(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP);
+		commonOutput.setDescription("Common Output Folder");
+		commonOutput.setOutputDirectory(commonDir);
+		commonOutput.setCreateOutputDirectory(true);
+		outputs.put(IFileSystemAccess.DEFAULT_OUTPUT, commonOutput);
 
-	    public static FPreferencesWamp getInstance() {
-	        if (instance == null) {
-	            instance = new FPreferencesWamp();
-	        }
-	        return instance;
-	    }
+		OutputConfiguration proxyOutput = new OutputConfiguration(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP);
+		proxyOutput.setDescription("Proxy Output Folder");
+		proxyOutput.setOutputDirectory(outputProxyDir);
+		proxyOutput.setCreateOutputDirectory(true);
+		outputs.put(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, proxyOutput);
 
-	    private void clidefPreferences(){
+		OutputConfiguration stubOutput = new OutputConfiguration(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP);
+		stubOutput.setDescription("Stub Output Folder");
+		stubOutput.setOutputDirectory(outputStubDir);
+		stubOutput.setCreateOutputDirectory(true);
+		outputs.put(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, stubOutput);
 
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP, PreferenceConstantsWamp.DEFAULT_OUTPUT);
-	        }    	
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, PreferenceConstantsWamp.DEFAULT_OUTPUT);
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, PreferenceConstantsWamp.DEFAULT_OUTPUT);
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP, PreferenceConstants.DEFAULT_OUTPUT);
-	        }    
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_OUTPUT_SUBDIRS_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_OUTPUT_SUBDIRS_WAMP, "false");
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_LICENSE_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_LICENSE_WAMP, PreferenceConstantsWamp.DEFAULT_LICENSE);
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_COMMON_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_GENERATE_COMMON_WAMP, "true");
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_STUB_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_GENERATE_STUB_WAMP, "true");
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_PROXY_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_GENERATE_PROXY_WAMP, "true");
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_LOGOUTPUT_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_LOGOUTPUT_WAMP, "true");
-	        }	        
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_CODE_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_GENERATE_CODE_WAMP, "true");    
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_DEPENDENCIES_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_GENERATE_DEPENDENCIES_WAMP, "true");    
-	        }
-	        if (!preferences.containsKey(PreferenceConstantsWamp.P_GENERATE_SYNC_CALLS_WAMP)) {
-	            preferences.put(PreferenceConstantsWamp.P_GENERATE_SYNC_CALLS_WAMP, "true");    
-	        }
-	    }
-
-	    public String getPreference(String preferencename, String defaultValue) {
-	    	
-	    	if (preferences.containsKey(preferencename)) {
-	    		return preferences.get(preferencename);
-	    	}
-	    	System.err.println("Unknown preference " + preferencename);
-	        return "";
-	    }
-
-	    public void setPreference(String name, String value) {
-	        if(preferences != null) {
-	        	preferences.put(name, value);
-	        }
-	    }
-
-	    public String getModelPath(FModel model) {
-	        String ret = model.eResource().getURI().toString();
-	        return ret;
-	    }
-	    
-	    /**
-	     * Set the output path configurations (based on stored the preference values) for file system access types 
-	     * (instance of AbstractFileSystemAccess)
-	     * @return
-	     */
-	    public HashMap<String, OutputConfiguration> getOutputpathConfiguration() {
-	        return getOutputpathConfiguration(null);
-	    }
-
-	    /**
-	     * Set the output path configurations (based on stored the preference values) for file system access types
-	     * (instance of AbstractFileSystemAccess)
-			 * @subdir the subdir to use, can be null
-	     * @return
-	     */
-	    public HashMap<String, OutputConfiguration> getOutputpathConfiguration(String subdir) {
-
-	        String defaultDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_DEFAULT_WAMP, PreferenceConstants.DEFAULT_OUTPUT);
-	        String commonDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP, defaultDir);
-	        String outputProxyDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, defaultDir);
-	        String outputStubDir = getPreference(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, defaultDir);
-
-	        if (null != subdir && getPreference(PreferenceConstants.P_OUTPUT_SUBDIRS, "false").equals("true")) {
-	            defaultDir = new File(defaultDir, subdir).getPath();
-	            commonDir = new File(commonDir, subdir).getPath();
-	            outputProxyDir = new File(outputProxyDir, subdir).getPath();
-	            outputStubDir = new File(outputStubDir, subdir).getPath();
-	        }
-
-	        HashMap<String, OutputConfiguration>  outputs = new HashMap<String, OutputConfiguration> ();
-	        
-	        OutputConfiguration commonOutput = new OutputConfiguration(PreferenceConstantsWamp.P_OUTPUT_COMMON_WAMP);
-	        commonOutput.setDescription("Common Output Folder");
-	        commonOutput.setOutputDirectory(commonDir);
-	        commonOutput.setCreateOutputDirectory(true);
-	        outputs.put(IFileSystemAccess.DEFAULT_OUTPUT, commonOutput);
-	        
-	        OutputConfiguration proxyOutput = new OutputConfiguration(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP);
-	        proxyOutput.setDescription("Proxy Output Folder");
-	        proxyOutput.setOutputDirectory(outputProxyDir);
-	        proxyOutput.setCreateOutputDirectory(true);
-	        outputs.put(PreferenceConstantsWamp.P_OUTPUT_PROXIES_WAMP, proxyOutput);
-	        
-	        OutputConfiguration stubOutput = new OutputConfiguration(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP);
-	        stubOutput.setDescription("Stub Output Folder");
-	        stubOutput.setOutputDirectory(outputStubDir);
-	        stubOutput.setCreateOutputDirectory(true);
-	        outputs.put(PreferenceConstantsWamp.P_OUTPUT_STUBS_WAMP, stubOutput);
-	        
-	        return outputs;
-	    }
+		return outputs;
+	}
 
 }
